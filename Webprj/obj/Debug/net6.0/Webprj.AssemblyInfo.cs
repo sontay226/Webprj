f87de9d405c2a3f1474198eb94f5a338ec5a4860
sontay226 <phamsontay2206@gmail.com>
2025-04-11 21:19:55 +0700

    update footer

--- Webprj/obj/Debug/net6.0/Webprj.AssemblyInfo.cs
+++ Webprj/obj/Debug/net6.0/Webprj.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Webprj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ed9617788026a4af406d7b96709b3dbaefc4bab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f68114da19c70c67fcdcf6683d209e1d813ff7fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Webprj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Webprj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
