89e774f3002a8abeeb61061e876551770602538e
sontay226 <phamsontay2206@gmail.com>
2025-04-12 17:47:19 +0700

    update index view

--- Webprj/obj/Debug/net6.0/Webprj.AssemblyInfo.cs
+++ Webprj/obj/Debug/net6.0/Webprj.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Webprj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+646f4543c8750fb5d809d5a669582b4a14a0d969")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48b4426c28c512b6dcf0936869e9bb15596f44f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Webprj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Webprj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
